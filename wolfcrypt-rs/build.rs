5c8f1b47d6a32efa1d616cac5fb687f5b6b85cbb
gasbytes <reda.chouk.01@gmail.com>
2024-06-09 00:05:57 +0200

    some refactoring, the bindings seems still to be the wrong ones

--- wolfcrypt-rs/build.rs
+++ wolfcrypt-rs/build.rs
@@ -2,39 +2,51 @@ extern crate bindgen;
 
 use std::env;
 use std::path::PathBuf;
-use std::fs;
+use std::path::Path;
+
+/*
+ * wolfssl_src = ./wolfssl/
+ * wolfssl_include_dir = ./wolfssl/wolfssl/
+ * wolfssl_lib_dir = /usr/local/lib
+ * */
+
 
 fn main() {
-    // Tell cargo to look for shared libraries in the specified directory.
-    println!("cargo:rustc-link-search=./wolfssl/");
-
-    // Tell cargo to tell rustc to link the system wolfcrypt shared library
-    // (included in the wolfssl library).
-    println!("cargo:rustc-link-lib=wolfssl");
+    let wolfssl_lib_dir = Path::new(&"/usr/local/lib"); 
+    let wolfssl_include_dir = Path::new(&"wolfssl/wolfssl");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
-    let bindings = bindgen::Builder::default()
+    let builder = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
         .header("libraries.h")
+        // Include libraries.
+        .clang_arg(format!("-I{}", wolfssl_include_dir.to_str().unwrap()))
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        // Finish the builder and generate the bindings.
-        .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()));
+
+    let builder = [
+        "wolfssl/wolfssl/.*.h",
+        "wolfssl/wolfssl/wolfcrypt/.*.h",
+    ]
+    .iter()
+    .fold(builder, |b, p| {
+        b.allowlist_file(wolfssl_include_dir.join(p).to_str().unwrap())
+    });
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let bindings: bindgen::Bindings = builder.generate().expect("Unable to generate bindings");
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 
-    // For convenience, we copy the content of bindings.rs into the new file
-    // inside the src directory from the OUT_DIR.
-    fs::copy(out_path.join("bindings.rs"), "./src/bindings.rs")
-        .expect("Couldn't copy bindings to src directory!");
+    println!(
+        "cargo:rustc-link-search=native={}",
+        wolfssl_lib_dir.to_str().unwrap()
+    );
+    println!("cargo:rustc-link-lib=libwolfssl");
 }
