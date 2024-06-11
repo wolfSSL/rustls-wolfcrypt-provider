3486692c552858109b569516e6a66c8d51e9e7e4
gasbytes <reda.chouk.01@gmail.com>
2024-06-11 20:33:55 +0200

    All the type errors seems to be fixed, sanity check completed, fails for some reason, looking into that before going to start writing the ChaCha Wrapper.

--- wolfcrypt-rs/src/lib.rs
+++ wolfcrypt-rs/src/lib.rs
@@ -17,12 +17,15 @@ mod tests {
     #[test]
     fn rsa_encrypt_decrypt() {
         let mut rng: WC_RNG = unsafe { mem::zeroed() };
+        let rng_ptr: *mut bindings::WC_RNG = &mut rng;
         let mut rsa_key: RsaKey = unsafe { mem::zeroed() };
-        let mut input: String = "I use Turing Machines to ask questions".to_string();
+        let rsa_key_ptr: *mut bindings::RsaKey = &mut rsa_key;
+        let input: String = "I use Turing Machines to ask questions".to_string();
         let mut out: [u8; 256] = [0; 256];
+        let out_ptr: *mut u8 = out.as_mut_ptr();
         let mut plain: [u8; 256] = [0; 256];
-        let ret;
-
+        let plain_ptr: *mut u8 = plain.as_mut_ptr();
+        let mut ret;
 
         unsafe {
             ret = wc_InitRng(&mut rng);
@@ -30,16 +33,18 @@ mod tests {
                 panic!("Error while initializing RNG!");
             }
 
-            let mut input_u8: *const u8 = input.as_ptr() as *const u8;
+            let input_u8: *const u8 = input.as_ptr() as *const u8;
 
-            ret = wc_RsaPublicEncrypt_ex(input_u8, mem::size_of<String>(input), out, mem::size_of<u8>(out), &rsa_key, &rng, 
-                WC_RSA_OAEP_PAD.try_into().unwrap(), wc_HashType_WC_HASH_TYPE_SHA, WC_MGF1SHA1.try_into().unwrap(), None, 0);
+            ret = wc_RsaPublicEncrypt_ex(input_u8, mem::size_of_val(&*input_u8).try_into().unwrap(), out_ptr, mem::size_of_val(&out).try_into().unwrap(), rsa_key_ptr, rng_ptr, 
+                WC_RSA_OAEP_PAD.try_into().unwrap(), wc_HashType_WC_HASH_TYPE_SHA, WC_MGF1SHA1.try_into().unwrap(), 
+                std::ptr::null_mut(), 0);
             if ret < 0 {
                 panic!("Error while encrypting with RSA!");
             }
 
-            ret = wc_RsaPrivateDecrypt_ex(out, ret.try_into().unwrap(), plain, mem::size_of<u8>(plain), &rsa_key,
-                WC_RSA_OAEP_PAD.try_into().unwrap(), wc_HashType_WC_HASH_TYPE_SHA, WC_MGF1SHA1.try_into().unwrap(), None, 0);
+            ret = wc_RsaPrivateDecrypt_ex(out_ptr, ret.try_into().unwrap(), plain_ptr, mem::size_of_val(&out).try_into().unwrap(), rsa_key_ptr,
+                WC_RSA_OAEP_PAD.try_into().unwrap(), wc_HashType_WC_HASH_TYPE_SHA, WC_MGF1SHA1.try_into().unwrap(), 
+                std::ptr::null_mut(), 0);
             if ret < 0 {
                 panic!("Error while decrypting with RSA!");
             }
