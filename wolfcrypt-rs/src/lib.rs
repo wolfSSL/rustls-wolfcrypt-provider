359685ff498b3628cc1ee9008d8c4e47945c7896
gasbytes <reda.chouk.01@gmail.com>
2024-06-11 17:39:45 +0200

    fixed most of the type errors from the conversion of the rsa C example to rust, still some of them left

--- wolfcrypt-rs/src/lib.rs
+++ wolfcrypt-rs/src/lib.rs
@@ -18,10 +18,11 @@ mod tests {
     fn rsa_encrypt_decrypt() {
         let mut rng: WC_RNG = unsafe { mem::zeroed() };
         let mut rsa_key: RsaKey = unsafe { mem::zeroed() };
-        let mut input: String = "I use Turing Machines to ask questions";
+        let mut input: String = "I use Turing Machines to ask questions".to_string();
         let mut out: [u8; 256] = [0; 256];
         let mut plain: [u8; 256] = [0; 256];
         let ret;
+
 
         unsafe {
             ret = wc_InitRng(&mut rng);
@@ -29,15 +30,17 @@ mod tests {
                 panic!("Error while initializing RNG!");
             }
 
-            ret = wc_RsaPublicEncrypt_ex(input, sizeof(input), out, sizeof(out), &rsa_key, &rng, 
-                WC_RSA_OAEP_PAD, WC_HASH_TYPE_SHA, WC_MGF1SHA1, NULL, 0);
-            if (ret < 0) {
+            let mut input_u8: *const u8 = input.as_ptr() as *const u8;
+
+            ret = wc_RsaPublicEncrypt_ex(input_u8, mem::size_of<String>(input), out, mem::size_of<u8>(out), &rsa_key, &rng, 
+                WC_RSA_OAEP_PAD.try_into().unwrap(), wc_HashType_WC_HASH_TYPE_SHA, WC_MGF1SHA1.try_into().unwrap(), None, 0);
+            if ret < 0 {
                 panic!("Error while encrypting with RSA!");
             }
 
-            ret = wc_RsaPublicDecrypt_ex(input, sizeof(input), out, sizeof(out), &rsa_key, &rng, 
-                WC_RSA_OAEP_PAD, WC_HASH_TYPE_SHA, WC_MGF1SHA1, NULL, 0);
-            if (ret < 0) {
+            ret = wc_RsaPrivateDecrypt_ex(out, ret.try_into().unwrap(), plain, mem::size_of<u8>(plain), &rsa_key,
+                WC_RSA_OAEP_PAD.try_into().unwrap(), wc_HashType_WC_HASH_TYPE_SHA, WC_MGF1SHA1.try_into().unwrap(), None, 0);
+            if ret < 0 {
                 panic!("Error while decrypting with RSA!");
             }
 
