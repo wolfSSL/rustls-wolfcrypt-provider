cb24f7889cfa877725b7997fc79c7ccde0fd5262
gasbytes <reda.chouk.01@gmail.com>
2024-06-12 12:54:37 +0200

    Rsa Sanity check now working.

--- wolfcrypt-rs/src/lib.rs
+++ wolfcrypt-rs/src/lib.rs
@@ -21,6 +21,7 @@ mod tests {
         let mut rsa_key: RsaKey = unsafe { mem::zeroed() };
         let rsa_key_ptr: *mut bindings::RsaKey = &mut rsa_key;
         let input: String = "I use Turing Machines to ask questions".to_string();
+        let input_u8: *const u8 = input.as_ptr() as *const u8;
         let mut out: [u8; 256] = [0; 256];
         let out_ptr: *mut u8 = out.as_mut_ptr();
         let mut plain: [u8; 256] = [0; 256];
@@ -28,28 +29,52 @@ mod tests {
         let mut ret;
 
         unsafe {
-            ret = wc_InitRng(&mut rng);
+            ret = wc_InitRsaKey(rsa_key_ptr, std::ptr::null_mut());
+            if ret != 0 {
+                panic!("Error while initializing Rsa key! Ret value: {}", ret);
+            }
+
+            ret = wc_InitRng(rng_ptr);
             if ret != 0 {
                 panic!("Error while initializing RNG!");
             }
 
-            let input_u8: *const u8 = input.as_ptr() as *const u8;
-
-            ret = wc_RsaPublicEncrypt_ex(input_u8, mem::size_of_val(&*input_u8).try_into().unwrap(), out_ptr, mem::size_of_val(&out).try_into().unwrap(), rsa_key_ptr, rng_ptr, 
-                WC_RSA_OAEP_PAD.try_into().unwrap(), wc_HashType_WC_HASH_TYPE_SHA, WC_MGF1SHA1.try_into().unwrap(), 
-                std::ptr::null_mut(), 0);
-            if ret < 0 {
-                panic!("Error while encrypting with RSA!");
+            ret = wc_RsaSetRNG(rsa_key_ptr, rng_ptr);
+            if ret != 0 {
+                panic!("Error while setting rng to Rsa key! Ret value: {}", ret);
             }
 
-            ret = wc_RsaPrivateDecrypt_ex(out_ptr, ret.try_into().unwrap(), plain_ptr, mem::size_of_val(&out).try_into().unwrap(), rsa_key_ptr,
-                WC_RSA_OAEP_PAD.try_into().unwrap(), wc_HashType_WC_HASH_TYPE_SHA, WC_MGF1SHA1.try_into().unwrap(), 
-                std::ptr::null_mut(), 0);
-            if ret < 0 {
-                panic!("Error while decrypting with RSA!");
+            ret = wc_MakeRsaKey(rsa_key_ptr, 1024, 65537, rng_ptr);
+            if ret != 0 {
+                panic!("Error while creating the Rsa Key! Ret value: {}", ret);
             }
 
-            assert_eq!(ret, 0);
+            ret = wc_RsaPublicEncrypt(
+                input_u8,
+                mem::size_of_val(&*input_u8).try_into().unwrap(),
+                out_ptr,
+                mem::size_of_val(&out).try_into().unwrap(),
+                rsa_key_ptr,
+                rng_ptr
+            );
+
+            if ret < 0 {
+                panic!("Error while encrypting with RSA! Ret value: {}", ret);
+            }
+
+            ret = wc_RsaPrivateDecrypt(
+                out_ptr,
+                ret.try_into().unwrap(),
+                plain_ptr,
+                mem::size_of_val(&plain).try_into().unwrap(),
+                rsa_key_ptr,
+            );
+
+            if ret < 0 {
+                panic!("Error while decrypting with RSA! Ret value: {}", ret);
+            }
+
+            assert!(ret > 0);
         }
     }
 }
