2cd409b95e51196c831ed6b1e8a3afe7cb49d539
gasbytes <reda.chouk.01@gmail.com>
2024-06-13 16:54:50 +0200

    removed useless variables

--- wolfcrypt-rs/src/lib.rs
+++ wolfcrypt-rs/src/lib.rs
@@ -14,38 +14,30 @@ mod tests {
     fn rsa_encrypt_decrypt() {
         unsafe {
             let mut rng: WC_RNG = mem::zeroed();
-            let rng_ptr: *mut bindings::WC_RNG = &mut rng;
-
             let mut rsa_key: RsaKey = mem::zeroed();
-            let rsa_key_ptr: *mut bindings::RsaKey = &mut rsa_key;
-
             let mut input: String = "I use Turing Machines to ask questions".to_string();
             let input_ptr: *mut u8 = input.as_mut_ptr();
             let input_length: word32 = input.len() as word32;
-
             let mut out: [u8; 256] = [0; 256];
-            let out_ptr: *mut u8 = out.as_mut_ptr();
-
             let mut plain: [u8; 256] = [0; 256];
-            let plain_ptr: *mut u8 = plain.as_mut_ptr();
 
             let mut ret;
-            ret = wc_InitRsaKey(rsa_key_ptr, std::ptr::null_mut());
+            ret = wc_InitRsaKey(&mut rsa_key, std::ptr::null_mut());
             if ret != 0 {
                 panic!("Error while initializing Rsa key! Ret value: {}", ret);
             }
 
-            ret = wc_InitRng(rng_ptr);
+            ret = wc_InitRng(&mut rng);
             if ret != 0 {
                 panic!("Error while initializing RNG!");
             }
 
-            ret = wc_RsaSetRNG(rsa_key_ptr, rng_ptr);
+            ret = wc_RsaSetRNG(&mut rsa_key, &mut rng);
             if ret != 0 {
                 panic!("Error while setting rng to Rsa key! Ret value: {}", ret);
             }
 
-            ret = wc_MakeRsaKey(rsa_key_ptr, 1024, 65537, rng_ptr);
+            ret = wc_MakeRsaKey(&mut rsa_key, 1024, 65537, &mut rng);
             if ret != 0 {
                 panic!("Error while creating the Rsa Key! Ret value: {}", ret);
             }
@@ -53,10 +45,10 @@ mod tests {
             ret = wc_RsaPublicEncrypt(
                 input_ptr,
                 input_length,
-                out_ptr,
+                out.as_mut_ptr(),
                 mem::size_of_val(&out).try_into().unwrap(),
-                rsa_key_ptr,
-                rng_ptr
+                &mut rsa_key,
+                &mut rng,
             );
 
             if ret < 0 {
@@ -64,11 +56,11 @@ mod tests {
             }
 
             ret = wc_RsaPrivateDecrypt(
-                out_ptr,
+                out.as_mut_ptr(),
                 ret.try_into().unwrap(),
-                plain_ptr,
+                plain.as_mut_ptr(),
                 mem::size_of_val(&plain).try_into().unwrap(),
-                rsa_key_ptr,
+                &mut rsa_key,
             );
 
             if ret < 0 {
@@ -82,8 +74,8 @@ mod tests {
 
             assert_eq!(plain_str.trim_end_matches('\0'), input_str);
 
-            wc_FreeRsaKey(rsa_key_ptr);
-            wc_FreeRng(rng_ptr);
+            wc_FreeRsaKey(&mut rsa_key);
+            wc_FreeRng(&mut rng);
         }
     }
 
