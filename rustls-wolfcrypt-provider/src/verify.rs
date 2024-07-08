122a301e0cbb3b83ae7fa138e77017d712aea203
gasbytes <reda.chouk.01@gmail.com>
2024-07-08 17:39:28 +0200

    removed PKCS1, for now focusing on supporting PSS only

--- rustls-wolfcrypt-provider/src/verify.rs
+++ rustls-wolfcrypt-provider/src/verify.rs
@@ -1,23 +1,21 @@
 use der::Reader;
 use rsa::signature::Verifier;
-use rsa::{pkcs1v15, pss, BigUint, RsaPublicKey};
+use rsa::{pss, BigUint, RsaPublicKey};
 use rustls::crypto::WebPkiSupportedAlgorithms;
 use rustls::pki_types::{AlgorithmIdentifier, InvalidSignature, SignatureVerificationAlgorithm};
 use rustls::SignatureScheme;
 use webpki::alg_id;
 use wolfcrypt_rs::*;
-use std::{mem, println};
+use std::mem;
 
 pub static ALGORITHMS: WebPkiSupportedAlgorithms = WebPkiSupportedAlgorithms {
-    all: &[RSA_PSS_SHA256, RSA_PKCS1_SHA256],
+    all: &[RSA_PSS_SHA256],
     mapping: &[
         (SignatureScheme::RSA_PSS_SHA256, &[RSA_PSS_SHA256]),
-        (SignatureScheme::RSA_PKCS1_SHA256, &[RSA_PKCS1_SHA256]),
     ],
 };
 
 static RSA_PSS_SHA256: &dyn SignatureVerificationAlgorithm = &RsaPssSha256Verify;
-static RSA_PKCS1_SHA256: &dyn SignatureVerificationAlgorithm = &RsaPkcs1Sha256Verify;
 
 #[derive(Debug)]
 struct RsaPssSha256Verify;
@@ -48,34 +46,6 @@ impl SignatureVerificationAlgorithm for RsaPssSha256Verify {
     }
 }
 
-#[derive(Debug)]
-struct RsaPkcs1Sha256Verify;
-
-impl SignatureVerificationAlgorithm for RsaPkcs1Sha256Verify {
-    fn public_key_alg_id(&self) -> AlgorithmIdentifier {
-        alg_id::RSA_ENCRYPTION
-    }
-
-    fn signature_alg_id(&self) -> AlgorithmIdentifier {
-        alg_id::RSA_PKCS1_SHA256
-    }
-
-    fn verify_signature(
-        &self,
-        public_key: &[u8],
-        message: &[u8],
-        signature: &[u8],
-    ) -> Result<(), InvalidSignature> {
-        let public_key = decode_spki_spk(public_key)?;
-
-        let signature = pkcs1v15::Signature::try_from(signature).map_err(|_| InvalidSignature)?;
-
-        pkcs1v15::VerifyingKey::<sha2::Sha256>::new(public_key)
-            .verify(message, &signature)
-            .map_err(|_| InvalidSignature)
-    }
-}
-
 fn decode_spki_spk(spki_spk: &[u8]) -> Result<RsaPublicKey, InvalidSignature> {
     // public_key: unfortunately this is not a whole SPKI, but just the key material.
     // decode the two integers manually.
@@ -94,11 +64,9 @@ fn decode_spki_spk(spki_spk: &[u8]) -> Result<RsaPublicKey, InvalidSignature> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::string::String;
-    use std::string::ToString;
 
     #[test]
-    fn test_verify() {
+    fn test_verify_pss() {
         unsafe {
             let mut rng: WC_RNG = mem::zeroed();
             let mut rsa_key: RsaKey = mem::zeroed();
