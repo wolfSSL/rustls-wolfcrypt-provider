3fb22214a24a56a18983b5d06d7a440dfea885ff
Reda Chouk <reda@wolfssl.com>
2024-09-18 19:48:35 +0200

    now using macro_rules, now it's wayyy more readable.

--- rustls-wolfcrypt-provider/src/types/types.rs
+++ rustls-wolfcrypt-provider/src/types/types.rs
@@ -2,146 +2,93 @@ use wolfcrypt_rs::*;
 use foreign_types::{ForeignType, ForeignTypeRef, Opaque};
 use std::ptr::NonNull;
 
-pub struct ECCKeyObjectRef(Opaque);
-unsafe impl ForeignTypeRef for ECCKeyObjectRef {
-    type CType = ecc_key;
-}
+macro_rules! define_foreign_type {
+    ($struct_name:ident, $ref_name:ident, $c_type:ty) => {
+        pub struct $ref_name(Opaque);
+        unsafe impl ForeignTypeRef for $ref_name {
+            type CType = $c_type;
+        }
 
-#[derive(Debug, Clone)]
-pub struct ECCKeyObject(NonNull<ecc_key>);
-unsafe impl Sync for ECCKeyObject{}
-unsafe impl Send for ECCKeyObject{}
-unsafe impl ForeignType for ECCKeyObject {
-    type CType = ecc_key;
+        #[derive(Debug, Clone)]
+        pub struct $struct_name(NonNull<$c_type>);
+        unsafe impl Sync for $struct_name{}
+        unsafe impl Send for $struct_name{}
+        unsafe impl ForeignType for $struct_name {
+            type CType = $c_type;
+            type Ref = $ref_name;
 
-    type Ref = ECCKeyObjectRef;
+            unsafe fn from_ptr(ptr: *mut Self::CType) -> Self {
+                Self(NonNull::new_unchecked(ptr))
+            }
 
-    unsafe fn from_ptr(ptr: *mut Self::CType) -> Self {
-        Self(NonNull::new_unchecked(ptr))
-    }
-
-    fn as_ptr(&self) -> *mut Self::CType {
-        self.0.as_ptr()
-    }
-}
-
-pub struct RsaKeyObjectRef(Opaque);
-unsafe impl ForeignTypeRef for RsaKeyObjectRef {
-    type CType = RsaKey;
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct RsaKeyObject(NonNull<RsaKey>);
-unsafe impl Sync for RsaKeyObject{}
-unsafe impl Send for RsaKeyObject{}
-unsafe impl ForeignType for RsaKeyObject {
-    type CType = RsaKey;
-
-    type Ref = RsaKeyObjectRef;
-
-    unsafe fn from_ptr(ptr: *mut Self::CType) -> Self {
-        Self(NonNull::new_unchecked(ptr))
-    }
-
-    fn as_ptr(&self) -> *mut Self::CType {
-        self.0.as_ptr()
-    }
-}
-
-pub struct WCRNGObjectRef(Opaque);
-unsafe impl ForeignTypeRef for WCRNGObjectRef {
-    type CType = WC_RNG;
-}
-
-pub struct  WCRNGObject(NonNull<WC_RNG>);
-unsafe impl Sync for WCRNGObject{}
-unsafe impl Send for WCRNGObject{}
-unsafe impl ForeignType for WCRNGObject {
-    type CType = WC_RNG;
-
-    type Ref = WCRNGObjectRef;
-
-    unsafe fn from_ptr(ptr: *mut Self::CType) -> Self {
-        Self(NonNull::new_unchecked(ptr))
-    }
-
-    fn as_ptr(&self) -> *mut Self::CType {
-        self.0.as_ptr()
-    }
-}
-impl Drop for WCRNGObject {
-    fn drop(&mut self) {
-        unsafe {
-            // Correctly free the RNG object.
-            let ret = wc_FreeRng(self.as_ptr());
-            if ret != 0 {
-                panic!("Error while freeing RNG!");
+            fn as_ptr(&self) -> *mut Self::CType {
+                self.0.as_ptr()
             }
         }
-    }
+    };
+    
+    // For types that also need Drop implementations
+    ($struct_name:ident, $ref_name:ident, $c_type:ty, drop($drop_fn:ident)) => {
+        define_foreign_type!($struct_name, $ref_name, $c_type);
+
+        impl Drop for $struct_name {
+            fn drop(&mut self) {
+                unsafe {
+                    let ret = $drop_fn(self.as_ptr());
+                    if ret != 0 {
+                        panic!("Error while freeing resource in Drop for {}", stringify!($struct_name));
+                    }
+                }
+            }
+        }
+    };
 }
 
-pub struct Curve25519KeyObjectRef(Opaque);
-unsafe impl ForeignTypeRef for Curve25519KeyObjectRef {
-    type CType = curve25519_key;
+macro_rules! define_foreign_type_with_copy {
+    ($struct_name:ident, $ref_name:ident, $c_type:ty) => {
+        pub struct $ref_name(Opaque);
+        unsafe impl ForeignTypeRef for $ref_name {
+            type CType = $c_type;
+        }
+
+        #[derive(Debug, Clone, Copy)]
+        pub struct $struct_name(NonNull<$c_type>);
+        unsafe impl Sync for $struct_name{}
+        unsafe impl Send for $struct_name{}
+        unsafe impl ForeignType for $struct_name {
+            type CType = $c_type;
+            type Ref = $ref_name;
+
+            unsafe fn from_ptr(ptr: *mut Self::CType) -> Self {
+                Self(NonNull::new_unchecked(ptr))
+            }
+
+            fn as_ptr(&self) -> *mut Self::CType {
+                self.0.as_ptr()
+            }
+        }
+    };
+
+    // For types that also need Drop implementations
+    ($struct_name:ident, $ref_name:ident, $c_type:ty, drop($drop_fn:ident)) => {
+        define_foreign_type_with_copy!($struct_name, $ref_name, $c_type);
+
+        impl Drop for $struct_name {
+            fn drop(&mut self) {
+                unsafe {
+                    let ret = $drop_fn(self.as_ptr());
+                    if ret != 0 {
+                        panic!("Error while freeing resource in Drop for {}", stringify!($struct_name));
+                    }
+                }
+            }
+        }
+    };
 }
-pub struct Curve25519KeyObject(NonNull<curve25519_key>);
-unsafe impl Sync for Curve25519KeyObject{}
-unsafe impl Send for Curve25519KeyObject{}
-unsafe impl ForeignType for Curve25519KeyObject {
-    type CType = curve25519_key;
 
-    type Ref = Curve25519KeyObjectRef;
-
-    unsafe fn from_ptr(ptr: *mut Self::CType) -> Self {
-        Self(NonNull::new_unchecked(ptr))
-    }
-
-    fn as_ptr(&self) -> *mut Self::CType {
-        self.0.as_ptr()
-    }
-}
-pub struct HmacObjectRef(Opaque);
-unsafe impl ForeignTypeRef for HmacObjectRef {
-    type CType = wolfcrypt_rs::Hmac;
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct HmacObject(NonNull<wolfcrypt_rs::Hmac>);
-unsafe impl Sync for HmacObject {}
-unsafe impl Send for HmacObject {}
-unsafe impl ForeignType for HmacObject {
-    type CType = wolfcrypt_rs::Hmac;
-
-    type Ref = HmacObjectRef;
-
-    unsafe fn from_ptr(ptr: *mut Self::CType) -> Self {
-        Self(NonNull::new_unchecked(ptr))
-    }
-
-    fn as_ptr(&self) -> *mut Self::CType {
-        self.0.as_ptr()
-    }
-}
-
-pub struct AesObjectRef(Opaque);
-unsafe impl ForeignTypeRef for AesObjectRef {
-    type CType = Aes;
-}
-#[derive(Debug, Clone, Copy)]
-pub struct AesObject(NonNull<Aes>);
-unsafe impl Sync for AesObject{}
-unsafe impl Send for AesObject{}
-unsafe impl ForeignType for AesObject {
-    type CType = Aes;
-
-    type Ref = AesObjectRef;
-
-    unsafe fn from_ptr(ptr: *mut Self::CType) -> Self {
-        Self(NonNull::new_unchecked(ptr))
-    }
-
-    fn as_ptr(&self) -> *mut Self::CType {
-        self.0.as_ptr()
-    }
-}
+define_foreign_type!(WCRNGObject, WCRNGObjectRef, WC_RNG, drop(wc_FreeRng));
+define_foreign_type!(Curve25519KeyObject, Curve25519KeyObjectRef, curve25519_key);
+define_foreign_type!(ECCKeyObject, ECCKeyObjectRef, ecc_key);
+define_foreign_type_with_copy!(RsaKeyObject, RsaKeyObjectRef, RsaKey);
+define_foreign_type_with_copy!(HmacObject, HmacObjectRef, wolfcrypt_rs::Hmac);
+define_foreign_type_with_copy!(AesObject, AesObjectRef, Aes);
