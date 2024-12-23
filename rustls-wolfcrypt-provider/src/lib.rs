e3cf4e45c9e08574c9d93f5c98ac3de19c94ad77
Reda Chouk <reda@wolfssl.com>
2024-12-23 23:06:56 +0100

    Completed PRF support.
    Replace separate SHA HMAC modules with unified enum-based solution
    for improved maintainability and type safety.
    Completed wolfcrypt PRF (Pseudo random function) backed implementation.

--- rustls-wolfcrypt-provider/src/lib.rs
+++ rustls-wolfcrypt-provider/src/lib.rs
@@ -15,6 +15,7 @@ mod kx;
 mod random;
 mod verify;
 mod prf;
+use crate::prf::WCPrfUsingHmac;
 pub mod aead {
     pub mod aes128gcm;
     pub mod aes256gcm;
@@ -34,11 +35,12 @@ pub mod hash {
 use crate::hash::{sha256, sha384};
 
 pub mod hmac {
-    pub mod sha256hmac;
-    pub mod sha384hmac;
+    pub mod hmac;
 }
-use crate::hmac::{sha256hmac, sha384hmac};
-mod types {
+
+use crate::hmac::hmac::WCShaHmac;
+
+pub mod types {
     pub mod types;
 }
 
@@ -150,7 +152,7 @@ pub static TLS13_CHACHA20_POLY1305_SHA256: rustls::SupportedCipherSuite =
             hash_provider: &sha256::WCSha256,
             confidentiality_limit: u64::MAX,
         },
-        hkdf_provider: &HkdfUsingHmac(&sha256hmac::WCSha256Hmac),
+        hkdf_provider: &HkdfUsingHmac(&WCShaHmac::Sha256),
         aead_alg: &chacha20::Chacha20Poly1305,
         quic: None,
     });
@@ -162,7 +164,7 @@ pub static TLS13_AES_128_GCM_SHA256: rustls::SupportedCipherSuite =
             hash_provider: &sha256::WCSha256,
             confidentiality_limit: 1 << 23,
         },
-        hkdf_provider: &HkdfUsingHmac(&sha256hmac::WCSha256Hmac),
+        hkdf_provider: &HkdfUsingHmac(&WCShaHmac::Sha256),
         aead_alg: &aes128gcm::Aes128Gcm,
         quic: None,
     });
@@ -174,7 +176,7 @@ pub static TLS13_AES_256_GCM_SHA384: rustls::SupportedCipherSuite =
             hash_provider: &sha384::WCSha384,
             confidentiality_limit: 1 << 23,
         },
-        hkdf_provider: &HkdfUsingHmac(&sha384hmac::WCSha384Hmac),
+        hkdf_provider: &HkdfUsingHmac(&WCShaHmac::Sha384),
         aead_alg: &aes256gcm::Aes256Gcm,
         quic: None,
     });
@@ -187,7 +189,7 @@ pub static TLS12_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256: rustls::SupportedCiphe
             confidentiality_limit: u64::MAX,
         },
         aead_alg: &chacha20::Chacha20Poly1305,
-        prf_provider: &rustls::crypto::tls12::PrfUsingHmac(&sha256hmac::WCSha256Hmac),
+        prf_provider: &WCPrfUsingHmac(WCShaHmac::Sha256),
         kx: rustls::crypto::KeyExchangeAlgorithm::ECDHE,
         sign: ALL_RSA_SCHEMES,
     });
@@ -200,7 +202,7 @@ pub static TLS12_ECDHE_RSA_WITH_AES_128_GCM_SHA256: rustls::SupportedCipherSuite
             confidentiality_limit: 1 << 23,
         },
         aead_alg: &aes128gcm::Aes128Gcm,
-        prf_provider: &rustls::crypto::tls12::PrfUsingHmac(&sha256hmac::WCSha256Hmac),
+        prf_provider: &WCPrfUsingHmac(WCShaHmac::Sha256),
         kx: rustls::crypto::KeyExchangeAlgorithm::ECDHE,
         sign: ALL_RSA_SCHEMES,
     });
@@ -213,7 +215,7 @@ pub static TLS12_ECDHE_RSA_WITH_AES_256_GCM_SHA384: rustls::SupportedCipherSuite
             confidentiality_limit: 1 << 23,
         },
         aead_alg: &aes256gcm::Aes256Gcm,
-        prf_provider: &rustls::crypto::tls12::PrfUsingHmac(&sha384hmac::WCSha384Hmac),
+        prf_provider: &WCPrfUsingHmac(WCShaHmac::Sha384),
         kx: rustls::crypto::KeyExchangeAlgorithm::ECDHE,
         sign: ALL_RSA_SCHEMES,
     });
@@ -225,7 +227,7 @@ pub static TLS12_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256: rustls::SupportedCip
             hash_provider: &sha256::WCSha256,
             confidentiality_limit: u64::MAX,
         },
-        prf_provider: &rustls::crypto::tls12::PrfUsingHmac(&sha256hmac::WCSha256Hmac),
+        prf_provider: &WCPrfUsingHmac(WCShaHmac::Sha256),
         kx: rustls::crypto::KeyExchangeAlgorithm::ECDHE,
         sign: ALL_ECDSA_SCHEMES,
         aead_alg: &chacha20::Chacha20Poly1305,
@@ -239,7 +241,7 @@ pub static TLS12_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256: rustls::SupportedCipherSui
             confidentiality_limit: 1 << 23,
         },
         aead_alg: &aes128gcm::Aes128Gcm,
-        prf_provider: &rustls::crypto::tls12::PrfUsingHmac(&sha256hmac::WCSha256Hmac),
+        prf_provider: &WCPrfUsingHmac(WCShaHmac::Sha256),
         kx: rustls::crypto::KeyExchangeAlgorithm::ECDHE,
         sign: ALL_ECDSA_SCHEMES,
     });
@@ -252,7 +254,7 @@ pub static TLS12_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384: rustls::SupportedCipherSui
             confidentiality_limit: 1 << 23,
         },
         aead_alg: &aes256gcm::Aes256Gcm,
-        prf_provider: &rustls::crypto::tls12::PrfUsingHmac(&sha384hmac::WCSha384Hmac),
+        prf_provider: &WCPrfUsingHmac(WCShaHmac::Sha384),
         kx: rustls::crypto::KeyExchangeAlgorithm::ECDHE,
         sign: ALL_ECDSA_SCHEMES,
     });
