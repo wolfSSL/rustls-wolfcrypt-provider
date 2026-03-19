9dc39dfbbf0626ffc3552c0db80711e2babf76c1
Reda Chouk <reda@wolfssl.com>
2026-03-19 12:01:10 +0100

    - added drop implementations for the missing foreign types to prevent potentiall resource leaking; - custom deallocation method for rsa to reclaim the Box allocation; - implemented Send and Sync to send the new rsa object safely between   threads;

--- rustls-wolfcrypt-provider/src/types/mod.rs
+++ rustls-wolfcrypt-provider/src/types/mod.rs
@@ -69,6 +69,16 @@ macro_rules! define_foreign_type {
             }
         }
     };
+
+    ($struct_name:ident, $ref_name:ident, $c_type:ty, drop_void($drop_fn:ident), $init_function:ident) => {
+        define_foreign_type!($struct_name, $ref_name, $c_type, $init_function);
+
+        impl Drop for $struct_name {
+            fn drop(&mut self) {
+                unsafe { $drop_fn(self.as_ptr()) };
+            }
+        }
+    };
 }
 
 macro_rules! define_foreign_type_with_copy {
@@ -123,6 +133,64 @@ macro_rules! define_foreign_type_with_copy {
     };
 }
 
+/// Like define_foreign_type_with_copy but without Copy (needed when Drop is implemented).
+macro_rules! define_foreign_type_no_copy {
+    ($struct_name:ident, $ref_name:ident, $c_type:ty) => {
+        pub struct $ref_name(Opaque);
+        unsafe impl ForeignTypeRef for $ref_name {
+            type CType = $c_type;
+        }
+
+        #[derive(Debug, Clone)]
+        pub struct $struct_name(NonNull<$c_type>);
+        unsafe impl Sync for $struct_name {}
+        unsafe impl Send for $struct_name {}
+        unsafe impl ForeignType for $struct_name {
+            type CType = $c_type;
+            type Ref = $ref_name;
+
+            unsafe fn from_ptr(ptr: *mut Self::CType) -> Self {
+                Self(NonNull::new_unchecked(ptr))
+            }
+
+            fn as_ptr(&self) -> *mut Self::CType {
+                self.0.as_ptr()
+            }
+        }
+    };
+
+    ($struct_name:ident, $ref_name:ident, $c_type:ty, drop($drop_fn:ident)) => {
+        define_foreign_type_no_copy!($struct_name, $ref_name, $c_type);
+
+        impl Drop for $struct_name {
+            fn drop(&mut self) {
+                let ret = unsafe { $drop_fn(self.as_ptr()) };
+                match check_if_zero(ret) {
+                    Err(err) => {
+                        error!(
+                            "Error while freeing resource in Drop for {}: {}",
+                            stringify!($struct_name),
+                            err
+                        );
+                    }
+                    Ok(()) => {}
+                }
+            }
+        }
+    };
+
+    ($struct_name:ident, $ref_name:ident, $c_type:ty, drop_void($drop_fn:ident)) => {
+        define_foreign_type_no_copy!($struct_name, $ref_name, $c_type);
+
+        impl Drop for $struct_name {
+            fn drop(&mut self) {
+                unsafe { $drop_fn(self.as_ptr()) };
+            }
+        }
+    };
+
+}
+
 define_foreign_type!(
     WCRngObject,
     WCRngObjectRef,
@@ -134,17 +202,19 @@ define_foreign_type!(
     Curve25519KeyObject,
     Curve25519KeyObjectRef,
     curve25519_key,
+    drop_void(wc_curve25519_free),
     wc_curve25519_init
 );
-define_foreign_type!(ECCKeyObject, ECCKeyObjectRef, ecc_key, wc_ecc_init);
+define_foreign_type!(ECCKeyObject, ECCKeyObjectRef, ecc_key, drop(wc_ecc_free), wc_ecc_init);
 define_foreign_type!(
     ED25519KeyObject,
     ED25519KeyObjectRef,
     ed25519_key,
+    drop_void(wc_ed25519_free),
     wc_ed25519_init
 );
-define_foreign_type!(ED448KeyObject, ED448KeyObjectRef, ed448_key, wc_ed448_init);
+define_foreign_type!(ED448KeyObject, ED448KeyObjectRef, ed448_key, drop_void(wc_ed448_free), wc_ed448_init);
 
-define_foreign_type_with_copy!(RsaKeyObject, RsaKeyObjectRef, RsaKey);
+define_foreign_type_no_copy!(RsaKeyObject, RsaKeyObjectRef, RsaKey, drop(wc_FreeRsaKey));
 define_foreign_type_with_copy!(HmacObject, HmacObjectRef, wolfcrypt_rs::Hmac);
-define_foreign_type_with_copy!(AesObject, AesObjectRef, Aes);
+define_foreign_type_no_copy!(AesObject, AesObjectRef, Aes, drop_void(wc_AesFree));
