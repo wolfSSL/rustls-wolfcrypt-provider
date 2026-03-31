3c40c7aa8b3f118825196f86c92aa7ff615433fa
Reda Chouk <reda@wolfssl.com>
2026-03-31 12:42:08 +0200

    add length check in ecdsa.rs before doing any slicing to parse and import ECC parameters.

--- rustls-wolfcrypt-provider/src/verify/ecdsa.rs
+++ rustls-wolfcrypt-provider/src/verify/ecdsa.rs
@@ -57,6 +57,7 @@ impl SignatureVerificationAlgorithm for EcdsaVerifier {
         message: &[u8],
         signature: &[u8],
     ) -> Result<(), InvalidSignature> {
+        // Verify length of the public key before doing any slicing.
         unsafe {
             // Initialize WolfSSL ECC key
             let mut ecc_c_type: ecc_key = mem::zeroed();
@@ -85,6 +86,11 @@ impl SignatureVerificationAlgorithm for EcdsaVerifier {
                 ),
                 _ => return Err(InvalidSignature),
             };
+
+            let expected_len = 1 + 2 * skip_len;
+            if public_key.len() != expected_len {
+                return Err(InvalidSignature);
+            }
 
             /*
              * Skipping first byte because rustls uses this format:
