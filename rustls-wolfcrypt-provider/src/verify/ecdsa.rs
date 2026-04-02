3c08b1691bf446bd00f05be1f6a03412a7517d03
Reda Chouk <reda@wolfssl.com>
2026-04-02 14:07:45 +0200

    bump rustls-webpki from 0.102 to 0.103.10

--- rustls-wolfcrypt-provider/src/verify/ecdsa.rs
+++ rustls-wolfcrypt-provider/src/verify/ecdsa.rs
@@ -5,7 +5,7 @@ use core::ptr;
 use foreign_types::ForeignType;
 use rustls::pki_types::{AlgorithmIdentifier, InvalidSignature, SignatureVerificationAlgorithm};
 use rustls::SignatureScheme;
-use webpki::alg_id;
+use rustls_pki_types::alg_id;
 use wolfcrypt_rs::*;
 
 /// A unified ECDSA verifier for P-256, P-384, and P-521.
